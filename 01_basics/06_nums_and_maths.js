aa3473a2bc3af894d687723bd428b7d2e7e1dac0
Priyanshow <hk8004426582@gmail.com>
2025-10-04 06:56:21 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 300
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(3));
+
+const otherNumber = 145.2564
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 2000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+////++++++++++++++++ Maths +++++++++++++++++////
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4, 5, 2, 7, 8));
+// console.log(Math.max(4, 5, 2, 7, 8));
+
+console.log(Math.random());
+console.log(Math.random()*100);
+console.log(Math.floor(Math.random()*100) + 2);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
