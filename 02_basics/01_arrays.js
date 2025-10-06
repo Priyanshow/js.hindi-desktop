d01320b9aa8d449e08b88f28f5bc3dabc3beb755
Priyanshow <hk8004426582@gmail.com>
2025-10-06 19:20:09 +0000

    About arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"];
+
+console.log(myArr[1]);
+// console.log(myHeros[0]);
+
+// Array nethods
+
+// myArr.push(6)                            ////push  //for add an array in the end
+// myArr.push(7)                            
+// myArr.pop()                              ////pop   //to remove an array from the end
+
+// myArr.unshift(8)                         ////unshift  //to add an array to the starting
+// myArr.shift()                            ////shift  //to remove the array from starting
+
+// console.log(myArr.includes(8));          ////includes  //true or false
+// console.log(myArr.indexOf(8));           ////indexof  //either -1 or as it is print
+
+// const newArr = myArr.join()              ////join 
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice, splice
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1, 3)              ////slice   //only print 1 and 2          
+
+// console.log(myn1);
+// console.log("B", myArr);
+
+
+// const myn2 = myArr.splice(1, 3)             ////splice  //can remove the given (1, 3)=>1,2,3 are removed
+// console.log("C", myArr);
+// console.log(myn2);
